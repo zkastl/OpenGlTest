25a68bff27ca438816b4caff626bc156130004b4
Zachary Kastl <zkastl8@gmail.com>
2020-03-23 10:21:23 -0500

    added script

---
+++ OpenTkTest/test.ps1
@@ -0,0 +1,17 @@
+﻿function CompareHashValue
+{
+    $file = $args[0]
+    $compare = $args[1]
+
+    #$file = '.\raytracer_master.bundle'
+    $hashval = (Get-FileHash $file).Hash
+    write-host $hashval
+}
+
+$input = Read-Host "Please enter the drive letter for your optical drive (e.g: D, E):"
+$opticaldrive = [System.IO.DriveInfo]::GetDrives() | where Name -Match $input
+$bundles =  [System.IO.DirectoryInfo]::new($opticaldrive).GetFiles() | where Name -Match ".bundle"
+$copylocation = ($env:USERPROFILE + '\Desktop\' + $bundles.Name)
+write-host "Copying to " + $copylocation
+cp $bundles.FullName $copylocation
+CompareHashValue $bundles.FullName
